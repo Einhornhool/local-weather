75e551440033005a76b3656f0b7155df6e80c531
Fehlerquelle <26007369+Fehlerquelle@users.noreply.github.com>
2018-04-27 07:45:10 +0200

    local weather update

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+if (navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#data").html("latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude);
+  });
+}
 
-  if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(function(position)) {
-      $("#data").html("latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude);
-  }
+$(document).ready(function() {
+  $.getJSON("https://fcc-weather-api.glitch.me/", function(json) {
+    $(".message").html(JSON.stringify(json));
+  });
 });
