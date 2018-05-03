6e6f0d586b0be62043e3e163e15b811ba46e0d68
Fehlerquelle <26007369+Fehlerquelle@users.noreply.github.com>
2018-05-03 19:44:45 +0200

    added some changes to make app work outside of codepen and images

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+getWeather();
 var APIkey = "4780efd2b8f8a268804dd13781a797d4";
 var loc;
 var cel = false;
@@ -6,11 +7,21 @@ var wd;
 function displayTemp(fTemp, c){
   if(c) return Math.round((fTemp - 32) * (5/9)) + " °C";
   return Math.round(fTemp) + " °F";
-}
+};
 
 function bgImage(wd) {
-  if (wd.weather.id = 800) {
-    $('body').css('background-image', 'url("https://images.unsplash.com/photo-1501682855625-15c985cd12a5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=24786866cf6aa9009096c706f1fd3454&auto=format&fit=crop&w=3450&q=80")');
+  if (wd.weather.id <= 232) {
+    $('body').css('background-image', 'url("images/thunderstorm.jpg")');
+  } else if (wd.weather.id >= 300 && wd.weather.id <= 531) {
+    $('body').css('background-image', 'url("images/rainy.jpg")');
+  } else if (wd.weather.id >= 600 && wd.weather.id <= 622) {
+    $('body').css('background-image', 'url("images/snowy.jpg")');
+  } else if (wd.weather.id >= 701 && wd.weather.id <= 781) {
+    $('body').css('background-image', 'url("images/foggy.jpg")');
+  } else if (wd.weather.id = 800) {
+    $('body').css('background-image', 'url("images/sunny.jpg")');
+  } else if (wd.weather.id >= 801 && wd.weather.id <= 804) {
+    $('body').css('background-image', 'url("images/cloudy.jpg")')
   };
 };
 
@@ -25,30 +36,30 @@ function render(wd, cel) {
   $('#currentTemp').html(currentTemp);
   $('#currentWeather').html(currentWeather);
   $('#minMax').html(tempMin + " | " + tempMax);
-  
+
   bgImage(wd);
-}
+};
 
-$(function(){
+function getWeather(){
 
-  if (navigator.geolocation) {    
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       loc = [position.coords.latitude, position.coords.longitude];
-      
+
       $.getJSON('https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + loc[0] + '&lon=' + loc[1] + '&APPID=' + APIkey, function(apiData) {
         wd = apiData;
 
         render(apiData, cel);
-        
+
         $('#cels').click(function(){
           cel = true;
           render(wd, cel);
         });
         $('#fahr').click(function(){
           cel = false;
-          render(wd, cel);          
+          render(wd, cel);
         });
       });
     });
-  };  
-});
+  };
+};
